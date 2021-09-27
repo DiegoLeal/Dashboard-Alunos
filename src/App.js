8b4f7e3e029b7a54436fb33ccb6a01693fc1adce
DiegoLeal <diegogmach@outlook.com>
2021-09-27 16:32:24 -0300

    Private Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Router } from 'react-router-dom';
+import history from "./services/history";
 import Header from "./components/Header";
 import Routes from "./routes";
 import GloblalStyles from "./styles/GloblalStyles";
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Header /> 
       <Routes />    
       <GloblalStyles />
