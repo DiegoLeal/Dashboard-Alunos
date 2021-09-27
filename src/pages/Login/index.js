f4bc302c6126ca8ab5a5274832b5ad62287dd0bc
DiegoLeal <diegogmach@outlook.com>
2021-09-27 00:33:52 -0300

    Login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "../../styles/GloblalStyles";
+import { Title } from "./styled";
+
+export default function Login() {
+    return (
+    <Container>
+        <Title>
+            Login
+        </Title>
+    </Container>
+    );
+}
