388d63d7d4093ad86d46377dcbfc92c70c882c01
DiegoLeal <diegogmach@outlook.com>
2021-09-27 17:08:08 -0300

    Axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Container } from "../../styles/GloblalStyles";
 import { Title } from "./styled";
+//import axios from "../../services/axios";
 
 export default function Login() {
+   /* React.useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/alunos');
+            const { data } = response;
+        }
+
+        getData();
+    }, []);*/
     return (
     <Container>
         <Title>
